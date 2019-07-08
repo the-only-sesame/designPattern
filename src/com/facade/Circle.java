1da92704ec02ae19dca7092ca739793460574069
Direct <Direct@Direct-PC>
2019-07-08 17:17:37 +0800

    Facade

---
+++ src/com/facade/Circle.java
@@ -0,0 +1,10 @@
+package com.facade;
+
+public class Circle implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("circle");
+	}
+
+}
---
+++ src/com/facade/Rectangle.java
@@ -0,0 +1,10 @@
+package com.facade;
+
+public class Rectangle implements Shape {
+
+	@Override
+	public void draw() {
+		System.out.println("shape");
+	}
+
+}

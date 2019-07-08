1495a43b3227451e2082ac6c8abfb96123fc1d4b
Direct <Direct@Direct-PC>
2019-07-08 14:08:12 +0800

    Prototype

---
+++ src/com/prototype/Rectangle.java
@@ -0,0 +1,13 @@
+package com.prototype;
+
+public class Rectangle extends Shape{
+	public Rectangle() {
+		type = "Rectangle";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("rectangle```");
+	}
+
+}
